f6b796515e14979895041e947d0cf83ad6544bda
Abdurrahmaan Iqbal <abdurrahmaaniqbal@hotmail.com>
2020-09-15 09:49:33 +0100

    Complete part 1 of the answer

--- answer.sql
+++ answer.sql
@@ -3,6 +3,17 @@
 -- PART 1: Create a SQL query that maps out the daily average users before and after the feature change
 
 
+-- Daily avg users (before)
+SELECT num / (julianday(datetime(1527897600, 'unixepoch')) - julianday(datetime(login_timestamp, 'unixepoch')))
+    FROM login_history
+    CROSS JOIN (SELECT count() AS num FROM login_history WHERE login_timestamp < 1527897600)
+    WHERE login_timestamp < 1527897600 LIMIT 1;
+
+-- Daily avg users (after)
+SELECT num / (julianday(datetime(login_timestamp, 'unixepoch')) - julianday(datetime(1527897600, 'unixepoch')))
+    FROM login_history
+    CROSS JOIN (SELECT count() AS num FROM login_history WHERE login_timestamp > 1527897600)
+    ORDER BY login_timestamp DESC LIMIT 1;
 
 
 -- PART 2: Create a SQL query that indicates the number of status changes by card

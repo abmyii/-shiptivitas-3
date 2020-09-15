9b10cacfb70103aab68cec4bb3d9238ddd65e419
Abdurrahmaan Iqbal <abdurrahmaaniqbal@hotmail.com>
2020-09-15 10:14:08 +0100

    Improve and save graphs

--- graphs.py
+++ graphs.py
@@ -4,16 +4,32 @@ import pandas as pd
 import sqlite3
 
 
-fig, axes = plt.subplots(nrows=2)
-
 conn = sqlite3.connect("shiptivity.db")
 df = pd.read_sql_query("SELECT * FROM login_history;", conn)
 
 df['login_timestamp'] = pd.to_datetime(df['login_timestamp'], unit='s')
-df.set_index('login_timestamp', inplace=True)
 
-ax = df[df.index < pd.to_datetime(1527897600, unit='s')]['user_id'].plot(ax=axes[0], title='Login frequency (Before)')
-ax2 = df[df.index > pd.to_datetime(1527897600, unit='s')]['user_id'].plot(ax=axes[1], title='Login frequency (After)')
+# Login frequency / Day
 
-fig.tight_layout()
+before = df[df.login_timestamp < pd.to_datetime(1527897600, unit='s')]
+ax = before.groupby(before.login_timestamp.dt.date).size().plot(title='Login frequency / Day')
+
+after = df[df.login_timestamp > pd.to_datetime(1527897600, unit='s')]
+after.groupby(after.login_timestamp.dt.date).size().plot(ax=ax, title='Login frequency / Day')
+
+ax.legend(['Login frequency / Day (Before)', 'Login frequency / Day (After)'])
+
+plt.show()
+
+
+# Unique users / Day
+
+before = df[df.login_timestamp < pd.to_datetime(1527897600, unit='s')].drop_duplicates(['user_id'])
+ax = before.groupby(before.login_timestamp.dt.date).size().plot(title='Unique users / Day')
+
+after = df[df.login_timestamp > pd.to_datetime(1527897600, unit='s')].drop_duplicates(['user_id'])
+after.groupby(after.login_timestamp.dt.date).size().plot(ax=ax, title='Unique users / Day')
+
+ax.legend(['Unique users / Day (Before)', 'Unique users / Day (After)'])
+
 plt.show()
